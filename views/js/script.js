d1b1882856cc9b8b1aeb3a5d630b2c39fedb2542
Ducaster <147523951+Ducaster@users.noreply.github.com>
2024-11-07 16:57:22 +0900

    Update script.js

--- views/js/script.js
+++ views/js/script.js
@@ -1,5 +1,5 @@
 const GUESTBOOK_API_ADDR = process.env.GUESTBOOK_API_ADDR;
-const BACKEND_URI = `${GUESTBOOK_API_ADDR}/users/login`;
+const BACKEND_URI = `http://${GUESTBOOK_API_ADDR}/users/login`;
 
 // 모달 열기
 function openModal() {
